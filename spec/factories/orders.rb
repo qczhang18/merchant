17e6efb2cdbd5a491a3500f81f5bc2cef36c54c4
Q. Zhang <aznyqjai@gmail.com>
2017-07-26 13:59:20 -0400

    generate scaffold Order

---
+++ spec/factories/orders.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :order do
+    user_id 1
+    status "MyString"
+  end
+end
---
+++ spec/views/orders/index.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "orders/index", type: :view do
+  before(:each) do
+    assign(:orders, [
+      Order.create!(
+        :user_id => 2,
+        :status => "Status"
+      ),
+      Order.create!(
+        :user_id => 2,
+        :status => "Status"
+      )
+    ])
+  end
+
+  it "renders a list of orders" do
+    render
+    assert_select "tr>td", :text => 2.to_s, :count => 2
+    assert_select "tr>td", :text => "Status".to_s, :count => 2
+  end
+end
