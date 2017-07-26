237a73f89d02b6e1f7c476c6ad228a61c29e13ad
Q. Zhang <aznyqjai@gmail.com>
2017-07-26 13:35:41 -0400

    printing stock with helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,5 +1,16 @@
 module ProductsHelper
+
   def print_price(price)
     number_to_currency price
   end
+
+  def print_stock(stock)
+    if stock > 0
+      content_tag(:span, "Stock: #{stock}", class: "in_stock")
+    else
+      content_tag(:span, "No Stock", class: "out_stock")
+   end
+  end
+
 end
+
