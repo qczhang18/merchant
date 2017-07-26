e7eda4dd0de7e8434a1e18e37b9e42c8648237a0
Q. Zhang <aznyqjai@gmail.com>
2017-07-26 15:58:37 -0400

    refactored load_order with find_or_initialize

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,34 +2,26 @@ class OrderItemsController < ApplicationController
   before_action :set_order_item, only: [:show, :edit, :update, :destroy]
   before_action :load_order, only: [:create]
 
-  # GET /order_items
-  # GET /order_items.json
   def index
     @order_items = OrderItem.all
   end
 
-  # GET /order_items/1
-  # GET /order_items/1.json
   def show
   end
 
-  # GET /order_items/new
   def new
     @order_item = OrderItem.new
   end
 
-  # GET /order_items/1/edit
   def edit
   end
 
-  # POST /order_items
-  # POST /order_items.json
   def create
-    @order_item = OrderItem.new(order_item_params)
+    @order_item = OrderItem.new(product_id: params[:product_id], order_id: @order.id )
 
     respond_to do |format|
       if @order_item.save
-        format.html { redirect_to @order_item, notice: 'Order item was successfully created.' }
+        format.html { redirect_to @order, notice: 'Successfully added product to cart' }
         format.json { render :show, status: :created, location: @order_item }
       else
         format.html { render :new }
@@ -38,8 +30,6 @@ class OrderItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /order_items/1
-  # PATCH/PUT /order_items/1.json
   def update
     respond_to do |format|
       if @order_item.update(order_item_params)
@@ -52,8 +42,6 @@ class OrderItemsController < ApplicationController
     end
   end
 
-  # DELETE /order_items/1
-  # DELETE /order_items/1.json
   def destroy
     @order_item.destroy
     respond_to do |format|
@@ -64,19 +52,17 @@ class OrderItemsController < ApplicationController
 
   private
     def load_order
-      begin
-        @order = Order.find(session[:order_id])
-      rescue ActiveRecord::RecordNotFound
-        @order = Order.create(status: "unsubmitted")
+      @order = Order.find_or_initialize_by_id(session[:order_id], status: "unsubmitted")
+      if @order.new_record?
+        @order.save!
         session[:order_id] = @order.id
       end
     end
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_order_item
       @order_item = OrderItem.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def order_item_params
       params.require(:order_item).permit(:product_id, :order_id, :quantity)
     end
