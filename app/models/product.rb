18b09a135c51759e163687e5e187b85bc118c338
Q. Zhang <aznyqjai@gmail.com>
2017-07-26 14:38:29 -0400

    building up relationship between product order orderitems

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   validates_numericality_of :price
   validates :stock, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  has_many :order_items
+
   def price=(input)
     input.delete!("$")
     super
