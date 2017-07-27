ea9d1c8023ad78904eb5f42a8a2fbf91a701028b
Q. Zhang <aznyqjai@gmail.com>
2017-07-27 13:50:28 -0400

    correctly flagging stock vs requested

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -4,11 +4,13 @@ module ProductsHelper
     number_to_currency price
   end
 
-  def print_stock(stock)
-    if stock > 0
-      content_tag(:span, "Stock: #{stock}", class: "in_stock")
-    else
+  def print_stock(stock, requested = 1)
+    if stock == 0
       content_tag(:span, "No Stock", class: "out_stock")
+    elsif stock > requested
+      content_tag(:span, "Stock: #{stock}", class: "in_stock")
+    elsif stock < requested
+      content_tag(:span, "Insufficient stock (#{stock})", class: "low_stock")
    end
   end
 
