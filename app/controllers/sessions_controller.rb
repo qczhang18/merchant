d3c469d46469ec993235199381bdf914143b32a1
Q. Zhang <aznyqjai@gmail.com>
2017-07-27 16:07:37 -0400

    login logout from root

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,12 @@
 class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_by_auth(request.env["omniauth.auth"])
-    render html: request.env["omniauth.auth"]
-    byebug
+    session[:user_id] = @user.id
+    redirect_to products_path, notice: "Login as #{@user.name}"
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to products_path, notice: "Successfuly Logout"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   match '/auth/:provider/callback', to: 'sessions#create', via: :get
+  match "/login" => redirect("/auth/twitter"), as: :login, via: :get
+  match "/logout" => "sessions#destroy", as: :logout, via: :get
 
   resources :orders
   resources :order_items
   resources :products
+  root 'products#index'
 end
